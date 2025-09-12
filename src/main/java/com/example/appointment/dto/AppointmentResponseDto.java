d1d2a17c37185e19b9f1d37ae80dd0639e22b304
김나영 <and___young@Nayoung-MacBookAir.local>
2025-09-12 10:07:09 +0900

    .

--- src/main/java/com/example/appointment/dto/AppointmentResponseDto.java
+++ src/main/java/com/example/appointment/dto/AppointmentResponseDto.java
@@ -1,7 +1,6 @@
 package com.example.appointment.dto;
 
 import com.example.appointment.entity.Appointment;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,32 +15,15 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class AppointmentResponseDto {
     
-    @JsonProperty("appointment_id")
     private String appointmentId;
-    
-    @JsonProperty("host_id")
     private String hostId;
-    
-    @JsonProperty("host_username")
     private String hostUsername;
-    
-    @JsonProperty("host_nickname")
     private String hostNickname;
-    
     private String title;
     private String description;
-    
-    @JsonProperty("start_time")
     private LocalDateTime startTime;
-    
-    @JsonProperty("end_time")
     private LocalDateTime endTime;
-    
-    @JsonProperty("location_id")
     private String locationId;
-    
-    @JsonProperty("appointment_status")
     private Appointment.AppointmentStatus appointmentStatus;
-    
     private String feedback; // 피드백 상태 (F: 미완료, T: 완료)
 }
